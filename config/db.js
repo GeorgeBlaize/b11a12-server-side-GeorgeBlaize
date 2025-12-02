122394bb1af2fd8cfbc7c7cf9a58853524e4815c
GeorgeBlaize <georgepurification@gmail.com>
2025-12-02 21:45:46 +0600

    db.js updated

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
-
+import dotenv from 'dotenv';
+dotenv.config(); 
 const connectDB = async () => {
   try {
     const client = new MongoClient(process.env.MONGO_URI, {
